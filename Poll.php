af162e08bda61ec1a82df44dd30b6e3d6d0cac04
mozukraken <mozukraken@gmail.com>
2018-05-07 23:55:23 +0900

    Pollクラスの作成

---
+++ Poll.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MyApp;
+
+class Poll {
+  private $_db;
+
+  public function __construct() {
+    $this->_connectDB();
+  }
+
+  private function _connectDB() {
+    try {
+      $this->_db = new \PDO(DSN, DB_USERNAME, DB_PASSWORD);
+      $this->_db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    } catch (\PDOException $e) {
+      throw new \Exception('Failed to connect DB');
+    }
+  }
+
+}
--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-ini_set('display_errors', 1);
-
-define('DSN', 'DSN');
-define('DB_USERNAME', 'DB_USERNAME');
-define('DB_PASSWORD', 'DB_PASSWORD');
-
-require_once(__DIR__ . '/functions.php');
